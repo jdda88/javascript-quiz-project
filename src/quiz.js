969bc3343b32f70b17eb97b3b8a424f3f83e3d2e
Juan David Diaz <jdda886@gmail.com>
2024-05-06 18:02:36 -0400

    corrections made

--- src/quiz.js
+++ src/quiz.js
@@ -21,14 +21,22 @@ class Quiz {
     shuffleQuestions(){
 
         for (let i = this.questions.length - 1; i > 0 ; i--){
-            const newQuestions = Math.floor(Math.random() * (i + 1));
+            const newQuestions = Math.floor(Math.random() * (i + 1));  //randomIndex re mane it
             [this.questions[i]], this.questions[newQuestions] = [this.questions[newQuestions], this.questions[i]]
-
+            ///[1, 2, 3, 4, 5]    [2, 1, 4, 3, 5]
     }}
 
     checkAnswer(answer) {
-        if(this.questions){  // how is this possible?
-            this.correctAnswers += 1
+        // if(this.questions){  // how is this possible? I don't know :O
+        //     this.correctAnswers += 1
+        // }
+
+        if(answer){
+            if(this.questions[this.currentQuestionIndex].answer === answer){
+                this.correctAnswers++
+            } 
+        } else {
+            alert("Please provide an answer")
         }
     }
 
