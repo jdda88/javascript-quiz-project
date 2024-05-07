505b6ed2a4e47a6ae4ac2cd64f8319195571916e
Juan David Diaz <jdda886@gmail.com>
2024-05-07 18:24:53 -0400

    day 2 quiz lab completed

--- src/quiz.js
+++ src/quiz.js
@@ -1,54 +1,85 @@
 class Quiz {
-    constructor (questions, timeLimit, timeRemaining) {
-        this.questions = questions
-        this.timeLimit = timeLimit
-        this.timeRemaining = timeRemaining
-        this.correctAnswers = 0
-        this.currentQuestionIndex = 0
-    }
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    getQuestion() {
-        return this.questions[this.currentQuestionIndex]
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+
+  moveToNextQuestion() {
+    this.currentQuestionIndex += 1;
+  }
+
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      const newQuestions = Math.floor(Math.random() * (i + 1)); //randomIndex re mane it
+      [this.questions[i]],
+        (this.questions[newQuestions] = [
+          this.questions[newQuestions],
+          this.questions[i],
+        ]);
+      ///[1, 2, 3, 4, 5]    [2, 1, 4, 3, 5]
     }
+  }
+
+  checkAnswer(answer) {
+    // if(this.questions){  // how is this possible? I don't know :O
+    //     this.correctAnswers += 1
+    // }
+
+    if (answer) {
+      if (this.questions[this.currentQuestionIndex].answer === answer) {
+        this.correctAnswers++;
+      }
+    } else {
+      alert("Please provide an answer");
+    }
+  }
+
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  filterQuestionsByDifficulty(difficulty){
     
-    moveToNextQuestion(){
+    // let randomDifficulty = Math.floor(Math.random() * 3) + 1 ;
+    if(difficulty >= 1 && difficulty <= 3) {
 
-    this.currentQuestionIndex += 1
-
+        this.questions = this.questions.filter((question) => {
+          return question.difficulty === difficulty
+        })
     }
+  }
+
+  // averageDifficulty() {
+
+  //      let avgDifficulty =  this.difficulty.reduce(function(acc, difficulty ){ acc + this.difficulty
+  //   }, 0)
+
+  //   let avgTotal = avgDifficulty / this.difficulty
+
+  //   return avgTotal
 
 
-    shuffleQuestions(){
-
-        for (let i = this.questions.length - 1; i > 0 ; i--){
-            const newQuestions = Math.floor(Math.random() * (i + 1));  //randomIndex re mane it
-            [this.questions[i]], this.questions[newQuestions] = [this.questions[newQuestions], this.questions[i]]
-            ///[1, 2, 3, 4, 5]    [2, 1, 4, 3, 5]
-    }}
-
-    checkAnswer(answer) {
-        // if(this.questions){  // how is this possible? I don't know :O
-        //     this.correctAnswers += 1
-        // }
-
-        if(answer){
-            if(this.questions[this.currentQuestionIndex].answer === answer){
-                this.correctAnswers++
-            } 
-        } else {
-            alert("Please provide an answer")
-        }
-    }
+    averageDifficulty() {
+      let sum =
+      this.questions.reduce((acc, question) => acc + question.difficulty, 0) 
+          let average = sum / this.questions.length;
+      return average
+  }
 
 
-
-   hasEnded(){
-    if (this.currentQuestionIndex < this.questions.length){
-        return false 
-    }
-    else{
-        return true 
-    }
-    
-   }
 }
+
+
+
+
